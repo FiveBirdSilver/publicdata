d214f7413c4db52de45492c0e55d71b9237a0c7b
O Se Eun <seo@goldenplanet.co.kr>
2022-07-20 16:15:48 +0900

    민지매니저님 .. 사랑해요 .....

--- page/Pohang/essential/Basic.js
+++ page/Pohang/essential/Basic.js
@@ -1,14 +1,24 @@
-import { View, Text, TouchableOpacity, ScrollView, Alert } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView, ActivityIndicator, Modal, Alert } from "react-native";
 import { useEffect, useState } from "react";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import axios from "axios";
+
+import TakePhoto from "../../component/TakePhoto";
+import { color } from "../../../assets/styles/color";
+import uploadImgToGcs from "../../component/util";
+
 import Input from "../../component/Input";
 import { styles } from "../../../assets/styles/add";
 
 export default function Basic({ route, navigation }) {
-  const { listName, listKey, teamKey } = route.params;
+  const { listName, listKey, region, regionKey, dataCollection, data, teamKey } = route.params;
+
   const API = "http://gw.tousflux.com:10307/PublicDataAppService.svc";
   const [value, setValue] = useState([]);
+  const [imageLength, setImageLength] = useState([]);
+  const [image, setImage] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
   useEffect(() => {
     axios
       .post(`${API}/api/pohang/essential/getbasic`, {
@@ -16,7 +26,19 @@ export default function Basic({ route, navigation }) {
         list_skey: listKey,
       })
       .then((res) => {
-        setValue(JSON.parse(res.data));
+        const response = JSON.parse(res.data);
+        let obj = response;
+
+        response.picture.forEach((v) => {
+          obj[v.name] = v.url;
+        });
+        setValue(obj);
+        setImageLength(
+          response.picture
+            .map((i) => i.url)
+            .filter((v) => v !== "")
+            .filter((o) => o !== null).length
+        );
       })
       .catch((err) => console.log(err));
   }, []);
@@ -27,35 +49,84 @@ export default function Basic({ route, navigation }) {
       [name]: text,
     }));
   };
+
+  const getImage = (uri, name) => {
+    const newArr = [...image];
+    let tmp = [...image];
+    console.log(name);
+    if (newArr.findIndex((v) => v.name === name) !== -1) {
+      tmp.forEach((v) => {
+        if (v.name === name) {
+          v.url = uri;
+        }
+      });
+    } else {
+      tmp.push({
+        name: name,
+        url: uri,
+      });
+    }
+    console.log(tmp);
+    setImage(tmp);
+    if (uri !== "") {
+      setImageLength(imageLength + 1);
+    } else if (uri === "") {
+      setImageLength(imageLength - 1);
+    }
+  };
+
   const handleOnSubmit = () => {
     if (
-      Object.values(value).filter((i) => i === null).length !== 0 ||
-      Object.values(value).filter((i) => i === "").length !== 0
+      value.e_b_touristDestination === null ||
+      value.e_b_address === null ||
+      value.e_b_fee === null ||
+      value.e_b_holiday === null ||
+      value.e_b_openingHours === null ||
+      value.e_b_travelTime === null ||
+      value.e_b_touristDestination === "" ||
+      value.e_b_address === "" ||
+      value.e_b_fee === "" ||
+      value.e_b_holiday === "" ||
+      value.e_b_openingHours === "" ||
+      value.e_b_travelTime === ""
     ) {
       Alert.alert("모든 항목을 입력해주세요.");
+    } else if (imageLength === 0) {
+      Alert.alert("필수 사진을 모두 추가해 주세요.");
     } else {
-      axios
-        .post(`${API}/api/pohang/essential/setbasic`, {
-          team_skey: teamKey,
-          list_skey: listKey,
-          e_b_touristDestination: value.e_b_touristDestination,
-          e_b_address: value.e_b_address,
-          e_b_travelTime: value.e_b_travelTime,
-          e_b_fee: value.e_b_fee,
-          e_b_openingHours: value.e_b_openingHours,
-          e_b_holiday: value.e_b_holiday,
-        })
-        .then((res) => {
-          const response = JSON.parse(res.data);
-          if (response.result === 1) {
-            Alert.alert("저장되었습니다.");
+      setModalVisible(true);
+      uploadImgToGcs(image, regionKey, region, listKey, dataCollection, data).then((result) => {
+        console.log("실행");
+        axios
+          .post(`${API}/api/pohang/essential/setbasic`, {
+            team_skey: teamKey,
+            list_skey: listKey,
+            e_b_touristDestination: value.e_b_touristDestination,
+            e_b_address: value.e_b_address,
+            e_b_travelTime: value.e_b_travelTime,
+            e_b_fee: value.e_b_fee,
+            e_b_openingHours: value.e_b_openingHours,
+            e_b_holiday: value.e_b_holiday,
+          })
+          .then((res) => {
+            const response = JSON.parse(res.data);
+            if (response.result === 1) {
+              setModalVisible(false);
+              Alert.alert("저장되었습니다.");
+              navigation.goBack();
+            } else {
+              setModalVisible(false);
+              Alert.alert("저장에 실패했습니다. 다시 시도해주세요.");
+              navigation.goBack();
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            setModalVisible(false);
+            Alert.alert("저장에 실패했습니다. 다시 시도해주세요.");
             navigation.goBack();
-          } else Alert.alert("저장에 실패했습니다. 다시 시도해주세요.");
-        })
-        .catch((err) => {
-          console.log(err);
-          Alert.alert("저장에 실패했습니다. 다시 시도해주세요.");
-        });
+          });
+      });
     }
   };
   return (
@@ -121,10 +192,29 @@ export default function Basic({ route, navigation }) {
                 value={value.e_b_holiday}
                 placeholder="EX. 화요일"
               />
+              <View style={styles.img}>
+                <TakePhoto
+                  title="관광지"
+                  name="p_e_basic_touristDestinationImg"
+                  getImage={getImage}
+                  value={value.p_e_basic_touristDestinationImg}
+                />
+              </View>
             </View>
           </View>
         </View>
       </View>
+      <Modal
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={[styles.modal, styles.horizontal]}>
+          <ActivityIndicator size="large" color={color.blue} />
+        </View>
+      </Modal>
     </ScrollView>
   );
 }
